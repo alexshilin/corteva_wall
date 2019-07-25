cf87a9cc20cfde0c055223b19df6cfe1ebbfc3b6
alexshilin <alex.shilin@gmail.com>
2019-07-25 00:16:08 -0400

    thumbnail mockup scene

---
+++ Corteva/Assets/ThumbnailMockups/SwitchContentItems.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchContentItems : MonoBehaviour {
+
+	public List<GameObject> items = new List<GameObject>();
+	private int showing = 0;
+	private Vector3 showPos = Vector3.zero;
+	private Vector3 hidePos = new Vector3 (0, -10, 0);
+
+	// Use this for initialization
+	void Start () {
+		SetItem (showing);
+	}
+
+	void SetItem(int _item){
+		for (int i = 0; i < items.Count; i++) {
+			if (i == _item) {
+				items [i].transform.position = showPos;
+			} else {
+				items [i].transform.position = hidePos;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			showing++;
+			if (showing == items.Count)
+				showing = 0;
+			SetItem (showing);
+		}
+	}
+}
